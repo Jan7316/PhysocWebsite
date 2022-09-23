2e0842a95d72effd1216d01ff130321fc880c106
Jan7316 <jckutos@gmail.com>
2022-09-23 19:12:25 +0100

    Updates

---
+++ precache-manifest.4efa583c88e7e080579357d981d1fb0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f299e10bd6ae7987cc77",
+    "url": "./static/css/main.9f04fb91.chunk.css"
+  },
+  {
+    "revision": "f299e10bd6ae7987cc77",
+    "url": "./static/js/main.382db682.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c49afef67e53313be248",
+    "url": "./static/js/2.4852acb9.chunk.js"
+  },
+  {
+    "revision": "1bd61bdd223498613f6bf79e2bd5e743",
+    "url": "./index.html"
+  }
+];
